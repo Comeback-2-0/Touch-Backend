84e1af13866c5b908cd3d239c0cdb5392a4265c6
IJ Roy <141866152+ij-roy@users.noreply.github.com>
2025-07-17 11:08:11 +0530

    AWS warning solved

--- utils/uploadToSpaces.js
+++ utils/uploadToSpaces.js
@@ -1,29 +1,34 @@
 // utils/uploadToSpaces.js
-const AWS = require('aws-sdk');
-const { v4: uuidv4 } = require('uuid');
-const path = require('path');
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { Upload } = require("@aws-sdk/lib-storage");
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
 
-const spacesEndpoint = new AWS.Endpoint(process.env.SPACES_ENDPOINT);
-
-const s3 = new AWS.S3({
-  endpoint: spacesEndpoint,
-  accessKeyId: process.env.SPACES_KEY,
-  secretAccessKey: process.env.SPACES_SECRET,
+const s3 = new S3Client({
+  endpoint: process.env.SPACES_ENDPOINT,
+  region: "sgp1",
+  credentials: {
+    accessKeyId: process.env.SPACES_KEY,
+    secretAccessKey: process.env.SPACES_SECRET,
+  },
 });
 
 const uploadToSpaces = async (fileBuffer, originalName, mimeType) => {
   const extension = path.extname(originalName);
   const key = `anonymous/${uuidv4()}${extension}`;
 
-  const params = {
-    Bucket: process.env.SPACES_BUCKET,
-    Key: key,
-    Body: fileBuffer,
-    ACL: 'public-read',
-    ContentType: mimeType,
-  };
+  const upload = new Upload({
+    client: s3,
+    params: {
+      Bucket: process.env.SPACES_BUCKET,
+      Key: key,
+      Body: fileBuffer,
+      ACL: "public-read",
+      ContentType: mimeType,
+    },
+  });
 
-  await s3.upload(params).promise();
+  await upload.done();
 
   return `${process.env.SPACES_CDN}/${key}`;
 };
